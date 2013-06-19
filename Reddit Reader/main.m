d25628fd3836f9399bc64bb83442183cecfad73e
cplain <coby.plain@gmail.com>
2013-06-20 09:31:02 +1000

    initial commit

---
+++ Reddit Reader/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Reddit Reader
+//
+//  Created by Coby Plain on 20/06/13.
+//  Copyright (c) 2013 seaplain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
