445de7093ee56e08e679c9fe69f460f23581ae5e
cplain <coby.plain@gmail.com>
2013-06-20 13:25:00 +1000

    decided to use a third party JSON library, added this in and displayed titles of threads in tableView

--- Reddit Reader/Classes/MainPage/MainPageViewController.h
+++ Reddit Reader/Classes/MainPage/MainPageViewController.h
@@ -10,4 +10,6 @@
 
 @interface MainPageViewController : UIViewController
 
+@property(nonatomic, retain)IBOutlet UITableView *tableView;
+
 @end
--- Reddit Reader/Classes/MainPage/MainPageViewController.m
+++ Reddit Reader/Classes/MainPage/MainPageViewController.m
@@ -9,6 +9,7 @@
 #define REDDIT_URL @"http://www.reddit.com"
 #define SPECIFIC_SUBREDDIT_URL @"/r/shittyaskscience/top.json"
 #import "MainPageViewController.h"
+#import "SBJson.h"
 
 @interface MainPageViewController ()
 @property (nonatomic, strong)NSMutableData *responseData;
@@ -17,12 +18,19 @@
 @implementation MainPageViewController
 
 NSMutableArray *mainDataArray;
+NSMutableArray *linkNamesArray;
 @synthesize responseData;
+@synthesize tableView;
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
     [self recieveJSON];
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [self populateList];
 }
 
 - (void)didReceiveMemoryWarning
@@ -57,14 +65,63 @@ NSMutableArray *mainDataArray;
 
 - (void)connectionDidFinishLoading:(NSURLConnection *)connection
 {
+    mainDataArray = [[NSMutableArray alloc]init];
+    
     NSLog(@"connectionDidFinishLoading");
     NSLog(@"Succeeded! Received %d bytes of data",[self.responseData length]);
     
     // convert to JSON
-    NSError *myError = nil;
-    mainDataArray = [NSJSONSerialization JSONObjectWithData:self.responseData options:NSJSONReadingMutableLeaves error:&myError];
+    SBJsonParser *objJson = [[SBJsonParser alloc] init];//
+    NSDictionary *data = (NSDictionary*)[objJson objectWithData:self.responseData];
+    NSDictionary *secondData = [data valueForKey:@"data"];
+    mainDataArray = [secondData valueForKey:@"children"];
     
-    NSLog(@"Array: %@",mainDataArray);
+    NSLog(@"Data: %@", data);
+    NSLog(@"Second Data: %@", secondData);
+    NSLog(@"Array: %@", mainDataArray);
+    
+    [self populateList];
+}
+
+-(void)populateList
+{
+    NSDictionary *tempDict;
+    linkNamesArray = [NSMutableArray array];
+    
+    for(int i = 0; i < [mainDataArray count]; i++)
+    {
+        tempDict = [[mainDataArray objectAtIndex:i]valueForKey:@"data"];
+        NSString *title = [tempDict valueForKey:@"title"];
+        [linkNamesArray addObject:title];
+    }
+    
+    NSLog(@"linkNamesArray: %@", linkNamesArray);
+    [self.tableView reloadData];
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [linkNamesArray count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)myTableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *simpleTableIdentifier = @"SimpleTableItem";
+    
+    UITableViewCell *cell = [myTableView dequeueReusableCellWithIdentifier:simpleTableIdentifier];
+    
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:simpleTableIdentifier];
+        [cell setAccessoryType:UITableViewCellAccessoryDisclosureIndicator];
+    }
+    
+    cell.textLabel.text = [linkNamesArray objectAtIndex:indexPath.row];
+    return cell;
+}
+
+- (void)tableView: (UITableView *)tableView didSelectRowAtIndexPath: (NSIndexPath *)indexPath
+{
+
 }
 
 @end
