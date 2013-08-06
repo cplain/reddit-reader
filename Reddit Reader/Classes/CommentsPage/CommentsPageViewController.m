88b2d4710fa125ab9d84e2022c639a6d6acce89e
cplain <coby.plain@gmail.com>
2013-08-06 10:25:24 +1000

    tidy up code in commentsViewController

--- Reddit Reader/Classes/CommentsPage/CommentsPageViewController.m
+++ Reddit Reader/Classes/CommentsPage/CommentsPageViewController.m
@@ -49,10 +49,6 @@ UIImageView *gifView;
         
         [self.popover presentPopoverFromBarButtonItem:self.myBarButtonItem permittedArrowDirections:UIPopoverArrowDirectionAny animated:YES];
     }
-    
-    [self.scrollview setContentSize:CGSizeMake(self.scrollview.frame.size.width, self.scrollview.frame.size.height)];
-    [self.scrollview setMinimumZoomScale:1.0];
-    [self.scrollview setMaximumZoomScale:4.0];
 }
 
 - (void)didReceiveMemoryWarning
@@ -87,58 +83,91 @@ UIImageView *gifView;
 -(void)loadThread
 {
     [self positionViews];
+    [self configScrollViewToPanImages];
     [self hideShadow:nil];
-    
     [myAlertView show];
-    ASIHTTPRequest *request = [ASIHTTPRequest requestWithURL:[NSURL URLWithString:self.thread.url]];
-    
-    [request setDelegate:self];
-    [request startAsynchronous];
+    [self sendRequest];
 }
 
 -(void)positionViews
 {
     self.threadName.text = self.thread.threadName;
-    self.containerView.frame = CGRectMake(self.containerView.frame.origin.x, self.containerView.frame.origin.y, self.containerView.frame.size.width, self.threadName.contentSize.height + SUBTLE_OFFSET);
-    self.tableView.frame = CGRectMake(self.tableView.frame.origin.x, self.containerView.frame.origin.y + self.containerView.frame.size.height - SUBTLE_OFFSET, self.tableView.frame.size.width, self.view.frame.size.height - self.containerView.frame.size.height + SUBTLE_OFFSET);
-    self.imageContainerView.frame = CGRectMake(self.imageContainerView.frame.origin.x, -(self.view.frame.size.height - self.containerView.frame.size.height + SUBTLE_OFFSET), self.imageContainerView.frame.size.width, self.view.frame.size.height - self.containerView.frame.size.height + SUBTLE_OFFSET);
+    self.containerView.frame      = CGRectMake(self.containerView.frame.origin.x,
+                                               self.containerView.frame.origin.y,
+                                               self.containerView.frame.size.width,
+                                               self.threadName.contentSize.height + SUBTLE_OFFSET);
+    
+    self.tableView.frame          = CGRectMake(self.tableView.frame.origin.x,
+                                               self.containerView.frame.origin.y + self.containerView.frame.size.height - SUBTLE_OFFSET,
+                                               self.tableView.frame.size.width,
+                                               self.view.frame.size.height - self.containerView.frame.size.height + SUBTLE_OFFSET);
+    
+    self.imageContainerView.frame = CGRectMake(self.imageContainerView.frame.origin.x,
+                                               -(self.view.frame.size.height - self.containerView.frame.size.height + SUBTLE_OFFSET),
+                                               self.imageContainerView.frame.size.width,
+                                               self.view.frame.size.height - self.containerView.frame.size.height + SUBTLE_OFFSET);
+}
+
+-(void)configScrollViewToPanImages
+{
+    [self.scrollview setContentSize:CGSizeMake(self.scrollview.frame.size.width, self.scrollview.frame.size.height)];
+    [self.scrollview setMinimumZoomScale:1.0];
+    [self.scrollview setMaximumZoomScale:4.0];
+}
+
+-(void)sendRequest
+{
+    ASIHTTPRequest *request = [ASIHTTPRequest requestWithURL:[NSURL URLWithString:self.thread.url]];
+    [request setDelegate:self];
+    [request startAsynchronous];
 }
 
 - (void)requestFinished:(ASIHTTPRequest *)request
 {
     [myAlertView dismissWithClickedButtonIndex:0 animated:YES];
-    
+    [self processComments:[self parseJSON:request]];
+    [self.tableView reloadData];
+    [self showImage];
+}
+
+-(NSMutableArray *)parseJSON:(ASIHTTPRequest *)request
+{
     SBJsonParser *objJson = [[SBJsonParser alloc] init];
     objJson.maxDepth = 10000; //The size of the JSON is so massive that the library's security measure kicks in, this stops it
-    
     NSMutableArray *JSONData = (NSMutableArray*)[objJson objectWithString:[request responseString]];
-    NSMutableArray *recievedComments = [[NSMutableArray alloc]initWithArray:[[[JSONData objectAtIndex:1] objectForKey:@"data"] objectForKey:@"children"]];
-    
+    return [[NSMutableArray alloc]initWithArray:[[[JSONData objectAtIndex:1] objectForKey:@"data"] objectForKey:@"children"]];
+}
+
+-(void)processComments:(NSMutableArray *)recievedComments
+{
     comments = [[NSMutableArray alloc]init];
-    Comment *tempComment;
     
     for (int i = 0; i < [recievedComments count]-1; i++)
     {
-        tempComment = [[Comment alloc]init];
-        tempComment.author = [[[recievedComments objectAtIndex:i] valueForKey:@"data"] valueForKey:@"author"];
-        tempComment.body = [[[recievedComments objectAtIndex:i] valueForKey:@"data"] valueForKey:@"body"];
-        tempComment.upvotes = [[[recievedComments objectAtIndex:i] valueForKey:@"data"] valueForKey:@"ups"];
-        tempComment.downvotes = [[[recievedComments objectAtIndex:i] valueForKey:@"data"] valueForKey:@"downs"];
-        tempComment.indent = 0;
-        
-        if ([[[[recievedComments objectAtIndex:i] valueForKey:@"data"] valueForKey:@"replies"] isKindOfClass:[NSDictionary class]])
-        {
-            tempComment.comments = [[NSMutableArray alloc]initWithArray:[[[[[recievedComments objectAtIndex:i] valueForKey:@"data"] valueForKey:@"replies"] valueForKey:@"data"] valueForKey:@"children"]];
-            [tempComment loadComments];
-            if (tempComment.comments != nil)
-                tempComment.isShowingComments = YES;
-        }
+        Comment *tempComment = [self loadComment:[recievedComments objectAtIndex:i]];
         [comments addObject:tempComment];
         [comments addObjectsFromArray:[tempComment getAllComments]];
     }
+}
+
+-(Comment *)loadComment:(NSDictionary *)commentDict
+{
+    Comment *tempComment;
+    tempComment = [[Comment alloc]init];
+    tempComment.author = [[commentDict valueForKey:@"data"] valueForKey:@"author"];
+    tempComment.body = [[commentDict valueForKey:@"data"] valueForKey:@"body"];
+    tempComment.upvotes = [[commentDict valueForKey:@"data"] valueForKey:@"ups"];
+    tempComment.downvotes = [[commentDict valueForKey:@"data"] valueForKey:@"downs"];
+    tempComment.indent = 0;
     
-    [self.tableView reloadData];
-    [self showImage];
+    if ([[[commentDict valueForKey:@"data"] valueForKey:@"replies"] isKindOfClass:[NSDictionary class]])
+    {
+        tempComment.comments = [[NSMutableArray alloc]initWithArray:[[[[commentDict valueForKey:@"data"] valueForKey:@"replies"] valueForKey:@"data"] valueForKey:@"children"]];
+        [tempComment loadComments];
+        if (tempComment.comments != nil)
+            tempComment.isShowingComments = YES;
+    }
+    return tempComment;
 }
 
 -(void)showImage
@@ -147,27 +176,33 @@ UIImageView *gifView;
         [gifView removeFromSuperview];
     
     if ([[self.thread.imageURL substringWithRange:NSMakeRange(self.thread.imageURL.length - 4, 4)]isEqualToString:@".gif"])
-    {
-        gifView = [AnimatedGif getAnimationForGifAtUrl: [NSURL URLWithString:self.thread.imageURL]];
-        [self.imageContainerView addSubview:gifView];
-        [self.imageView setHidden:YES];
-        [self showAnimation];
-    }
+        [self loadGif];
     else
-    {
-        //Reddit can just give straight links to imgur.com - when this happens, to get the actual image you need to add a filetype
-        if (![[self.thread.imageURL substringWithRange:NSMakeRange(self.thread.imageURL.length - 4, 4)]isEqualToString:@".jpg"])
-            self.imageView.image = [self fetchImage:[NSString stringWithFormat:@"%@.jpg", self.thread.imageURL]];
-        else
-            self.imageView.image = [self fetchImage:self.thread.imageURL];
-        
-        [self.imageView setHidden:NO];
-        
-        if (self.imageView.image != nil)
-            [self showAnimation];
-    }
+        [self loadImage];
     
     [self.imageLabel setHidden:(self.imageView.image == nil)];
+}
+
+-(void)loadGif
+{
+    gifView = [AnimatedGif getAnimationForGifAtUrl: [NSURL URLWithString:self.thread.imageURL]];
+    [self.imageContainerView addSubview:gifView];
+    [self.imageView setHidden:YES];
+    [self showAnimation];
+}
+
+-(void)loadImage
+{
+    //Reddit can just give straight links to imgur.com - when this happens, to get the actual image you need to add a filetype
+    if (![[self.thread.imageURL substringWithRange:NSMakeRange(self.thread.imageURL.length - 4, 4)]isEqualToString:@".jpg"])
+        self.imageView.image = [self fetchImage:[NSString stringWithFormat:@"%@.jpg", self.thread.imageURL]];
+    else
+        self.imageView.image = [self fetchImage:self.thread.imageURL];
+    
+    [self.imageView setHidden:NO];
+    
+    if (self.imageView.image != nil)
+        [self showAnimation];
 }
 
 -(UIImage *)fetchImage:(NSString *)urlString
@@ -229,7 +264,6 @@ UIImageView *gifView;
     Comment *comment = [comments objectAtIndex:indexPath.row];
     
     cell.containerView.frame = CGRectMake(cell.frame.origin.x + comment.indent, cell.containerView.frame.origin.y, cell.frame.size.width - comment.indent, cell.containerView.frame.size.height);
-    
     cell.mainTextView.text = comment.body;
     cell.upvotes.text =  [NSString stringWithFormat:@"%@", comment.upvotes] ;
     cell.downvotes.text = [NSString stringWithFormat:@"%@", comment.downvotes];
@@ -250,24 +284,29 @@ UIImageView *gifView;
     if (comment == nil) return;
     
     if(comment.isShowingComments)
-    {
-        for (int i = 0; i < [comment.comments count]-1; i++)
-        {
-            [self removeSubComments:[comments objectAtIndex:indexPath.row+1] atPosition:indexPath.row+1];
-            [comments removeObjectAtIndex:indexPath.row + 1];
-        }
-    }
+        [self collapse:comment atIndex:indexPath.row];
     else
-    {
-        if ([comment.comments count] == 0) return;
-        
-        for (int i = 0; i < [comment.comments count]-1; i++)
-        {
-            [comments insertObject:[comment.comments objectAtIndex:i] atIndex:(indexPath.row + 1 + i)];
-        }
-    }
+        [self expand:comment atIndex:indexPath.row];
+    
     comment.isShowingComments = !comment.isShowingComments;  
     [tableView reloadData];
+}
+
+-(void)collapse:(Comment*)comment atIndex:(int)index
+{
+    for (int i = 0; i < [comment.comments count]-1; i++)
+    {
+        [self removeSubComments:[comments objectAtIndex:index+1] atPosition:index+1];
+        [comments removeObjectAtIndex:index + 1];
+    }
+}
+
+-(void)expand:(Comment *) comment atIndex:(int)index
+{
+    if ([comment.comments count] == 0) return;
+    
+    for (int i = 0; i < [comment.comments count]-1; i++)
+        [comments insertObject:[comment.comments objectAtIndex:i] atIndex:(index + 1 + i)];
 }
 
 -(void)removeSubComments:(Comment *)comment atPosition:(NSUInteger)currentPosition
@@ -289,14 +328,18 @@ UIImageView *gifView;
 }
 
 -(float)getCellContentHeight:(NSIndexPath *)indexPath
+{    
+    return 115.0f - 58.0f + [self sizeOfComment:[comments objectAtIndex:indexPath.row]];
+}
+
+-(int)sizeOfComment:(Comment *) comment
 {
-    Comment *tempComment = [comments objectAtIndex:indexPath.row];    
     UIView *tempView = [[UIView alloc]init];
     UITextView *tempTextView = [[UITextView alloc]initWithFrame:CGRectMake(0, 0, (self.tableView.contentSize.width), 5)];
-    tempTextView.text = tempComment.body;
+    tempTextView.text = comment.body;
     tempTextView.font = [UIFont fontWithName:@"Helvetica" size:14];
     [tempView addSubview:tempTextView];
-    return 115.0f - 58.0f + tempTextView.contentSize.height;
+    return tempTextView.contentSize.height;
 }
 
 -(IBAction)topCommentTapped:(id)sender
@@ -313,7 +356,10 @@ UIImageView *gifView;
     self.view.userInteractionEnabled = NO;
     [UIView animateWithDuration:ANIMATION_DURATION
                      animations:^{
-                         self.imageContainerView.frame = CGRectMake(self.imageContainerView.frame.origin.x, self.containerView.frame.origin.y + self.containerView.frame.size.height - SUBTLE_OFFSET, self.imageView.frame.size.width, self.imageContainerView.frame.size.height);
+                         self.imageContainerView.frame = CGRectMake(self.imageContainerView.frame.origin.x,
+                                                                    self.containerView.frame.origin.y + self.containerView.frame.size.height - SUBTLE_OFFSET,
+                                                                    self.imageView.frame.size.width,
+                                                                    self.imageContainerView.frame.size.height);
                      }];
     [self performSelector:@selector(restoreTouch:) withObject:nil afterDelay:(NSTimeInterval)ANIMATION_DURATION];
 }
@@ -334,7 +380,10 @@ UIImageView *gifView;
     self.view.userInteractionEnabled = NO;
     [UIView animateWithDuration:ANIMATION_DURATION
                      animations:^{
-                         self.imageContainerView.frame = CGRectMake(self.imageContainerView.frame.origin.x, -self.imageContainerView.frame.size.height, self.imageContainerView.frame.size.width, self.imageContainerView.frame.size.height);
+                         self.imageContainerView.frame = CGRectMake(self.imageContainerView.frame.origin.x,
+                                                                    -self.imageContainerView.frame.size.height,
+                                                                    self.imageContainerView.frame.size.width,
+                                                                    self.imageContainerView.frame.size.height);
                      }];
     
     [self performSelector:@selector(hideShadow:) withObject:nil afterDelay:(NSTimeInterval)ANIMATION_DURATION];
